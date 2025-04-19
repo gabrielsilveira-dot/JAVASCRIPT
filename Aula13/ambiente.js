1126684c8bd4956d5bf27b3db9e13f7e24bc64ec
gabrielsilveira-dot <adeiltongabriel@gmail.com>
2025-04-18 21:00:38 -0300

    13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,27 @@
+//TODO:
+// while (enquanto) - loop
+// while (condição) { bloco de código } 
+// while (true) { bloco de código } - loop infinito
+// while (false) { bloco de código } - loop nunca executado
+// while (true) { console.log('loop infinito') } - loop infinito        
+
+
+// var i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+//TODO:
+// do while (faça enquanto) - loop
+// do { bloco de código } while (condição)  
+// do { bloco de código } while (true) - loop infinito
+// do { bloco de código } while (false) - loop nunca executado
+
+
+// var i = 0;
+//  do{
+//     console.log(i);
+//     i++;
+// } while (i <= 10)    
